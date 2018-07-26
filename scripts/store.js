71c73118c4c5dca5adc99e076a3e228cb6adf194
jonsung89 <jonsung89@gmail.com>
2018-07-26 12:06:28 -0500

    added store

--- scripts/store.js
+++ scripts/store.js
@@ -1,2 +1,24 @@
 'use strict';
-/* global $ */
+/* global $ cuid*/
+/* eslint-disable-next-line no-unused-vars */
+
+const STORE = (function() {
+
+  const bookmarks = [{
+    id: cuid(),
+    title: 'Article on jQuery',
+    description: 'Lorem ipsum dolor sit amet',
+    rating: 5, 
+    expanded: true,
+  }];
+
+  const adding= false;
+  const error= false;
+
+  return {
+    bookmarks,
+    adding,
+    error
+  };
+
+}());
